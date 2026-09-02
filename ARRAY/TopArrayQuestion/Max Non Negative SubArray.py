ff94007667ead1bb8983441770bb2bf472d80186
Maaz qureshi <maazqureshi161@gmail.com>
2026-09-02 10:43:29 +0300

    Add max non-negative subarray solution
    This change adds JavaScript and Python implementations for the Max Non Negative SubArray problem and updates the related array study materials. It also removes the temporary Word lock file generated during editing.

---
+++ ARRAY/TopArrayQuestion/Max Non Negative SubArray.py
@@ -0,0 +1,30 @@
+def Max_SubArray(num):
+    sub_array = []
+    current = []
+    for i in num:
+        if i < 0:
+            if current:
+                sub_array.append(current)
+                current = []
+        else:
+            current.append(i)
+    # Add the last group 
+    if current:
+        sub_array.append(current)
+    if not sub_array:
+        return []
+    max_sum = -1
+    best_index = 0
+    # Find the subarray with the biggest sum
+    for i in range(len(sub_array)):
+        current_sum = 0
+        for j  in sub_array[i]:
+            current_sum += j
+        if current_sum > max_sum:
+            max_sum = current_sum
+            best_index = i
+    return sub_array[i]
+
+
+
+print(Max_SubArray([1, 2, 5, -7, 2, 3]))
